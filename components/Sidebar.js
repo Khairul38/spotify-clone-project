ad64c0a4dff837e95ca1ff29266f7fbee1ac44be
Khairul38 <khairulislam38@gmail.com>
2022-01-09 01:12:50 +0600

    update

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,14 +1,33 @@
 import { LogoutIcon, HomeIcon, SearchIcon, LibraryIcon, PlusCircleIcon, HeartIcon, RssIcon, } from '@heroicons/react/outline';
 import { signOut, useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { playlistIdState } from '../atoms/playlistAtom';
+import useSpotify from '../hooks/useSpotify';
 
 function Sidebar() {
-    const {data: session, status} = useSession();
+    const spotifyApi = useSpotify();
+    const { data: session, status } = useSession();
+    const [playlists, setPlaylists] = useState([]);
+    const [playlistId, setPlaylistId] = useRecoilState(playlistIdState);
+
+    // console.log(playlistId);
+
+    useEffect(() => {
+        if (spotifyApi.getAccessToken()) {
+            spotifyApi.getUserPlaylists().then(data => {
+                setPlaylists(data.body.items)
+            });
+        }
+    }, [session, spotifyApi])
+
+    // console.log(playlists);
 
     return (
         <div className='text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll h-screen scrollbar-hide'>
             <div className='space-y-4'>
                 <button className='flex items-center space-x-2 hover:text-white'
-                onClick={() => signOut()}
+                    onClick={() => signOut()}
                 >
                     <LogoutIcon className='h-5 w-5' />
                     <p>Logout</p>
@@ -42,24 +61,13 @@ function Sidebar() {
                 <hr className='border-t-[0.1px] border-gray-900' />
 
                 {/* Playlist... */}
-                <p className='cursor-pointer hover:text-white'>
-                    Playlist name....
-                </p>
-                <p className='cursor-pointer hover:text-white'>
-                    Playlist name....
-                </p>
-                <p className='cursor-pointer hover:text-white'>
-                    Playlist name....
-                </p>
-                <p className='cursor-pointer hover:text-white'>
-                    Playlist name....
-                </p>
-                <p className='cursor-pointer hover:text-white'>
-                    Playlist name....
-                </p>
-                <p className='cursor-pointer hover:text-white'>
-                    Playlist name....
-                </p>
+                {playlists.map(playlist => (
+                    <p key={playlist.id} 
+                     onClick={() => setPlaylistId(playlist.id)}
+                     className='cursor-pointer hover:text-white'>
+                        {playlist.name}
+                    </p>
+                ))}
             </div>
         </div>
     )
--- hooks/useSpotify.js
+++ hooks/useSpotify.js
@@ -1,6 +1,12 @@
 import { useSession } from "next-auth/react";
 import { signIn, useEffect } from "react";
-import spotifyApi from "../lib/spotify";
+import SpotifyWebApi from "spotify-web-api-node";
+
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+    clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+});
 
 function useSpotify() {
     const { data: session, status } = useSession();
@@ -16,11 +22,7 @@ function useSpotify() {
         }
     }, [session]);
 
-    return (
-        <div>
-
-        </div>
-    )
+    return spotifyApi;
 }
 
 export default useSpotify
