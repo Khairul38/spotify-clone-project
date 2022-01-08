e01106159d09f952b9eb124d5caa135f608ae7cf
Khairul38 <khairulislam38@gmail.com>
2022-01-09 02:19:56 +0600

    update

--- components/Center.js
+++ components/Center.js
@@ -51,7 +51,7 @@ function Center() {
                 </div>
             </header>
 
-            <section className={`flex items-end spate-x-7 bg-gradient-to-b to-black ${color} h-80 text-white padding-8`}>
+            <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8`}>
                 <img className="h-44 w-44 shadow-2xl" src={playlist?.images?.[0]?.url} alt="" />
                 <div>
                     <p>PLAYLIST</p>
